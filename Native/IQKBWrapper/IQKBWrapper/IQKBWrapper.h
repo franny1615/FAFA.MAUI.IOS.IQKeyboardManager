9cc0aba3eab4da2e204c663f350ede253e73be5a
Francisco Funes <franke1615@gmail.com>
2025-02-19 18:55:45 -0800

    adding files

---
+++ Native/IQKBWrapper/IQKBWrapper/IQKBWrapper.h
@@ -0,0 +1,18 @@
+//
+//  IQKBWrapper.h
+//  IQKBWrapper
+//
+//  Created by Francisco on 2/19/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IQKBWrapper.
+FOUNDATION_EXPORT double IQKBWrapperVersionNumber;
+
+//! Project version string for IQKBWrapper.
+FOUNDATION_EXPORT const unsigned char IQKBWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IQKBWrapper/PublicHeader.h>
+
+
